18668db52ad06327f547f360afc62ca77e7cc37f
danCode-s <dancodes9@gmail.com>
2024-06-04 13:54:59 +0500

    basic plotting

--- matplotlib/intro.py
+++ matplotlib/intro.py
@@ -4,6 +4,14 @@ import numpy as np
 xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 
-plt.plot(xpoints, ypoints)
+# plt.plot(xpoints, ypoints)
+# plt.plot(xpoints, ypoints, 'o')
+
+
+xpoints_2 = np.array([1, 2, 6, 8])
+ypoints_2 = np.array([3, 8, 1, 10])
+
+#plt.plot(xpoints_2, ypoints_2)
+plt.plot(ypoints_2)
 
 plt.show()
